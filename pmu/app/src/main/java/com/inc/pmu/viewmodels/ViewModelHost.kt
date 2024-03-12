cccacabee4ec452ab1b43b2fe813d5d0b4adde9b
Axel C <axel.cazorla@laposte.net>
2024-03-12 16:34:37 +0100

    [feat] Host View Model added

---
+++ pmu/app/src/main/java/com/inc/pmu/viewmodels/ViewModelHost.kt
@@ -0,0 +1,96 @@
+package com.inc.pmu.viewmodels
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.google.android.gms.nearby.connection.AdvertisingOptions
+import com.google.android.gms.nearby.connection.ConnectionInfo
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback
+import com.google.android.gms.nearby.connection.ConnectionResolution
+import com.google.android.gms.nearby.connection.ConnectionsClient
+import com.google.android.gms.nearby.connection.ConnectionsStatusCodes
+import com.google.android.gms.nearby.connection.Payload
+import com.google.android.gms.nearby.connection.PayloadCallback
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate
+import com.google.android.gms.nearby.connection.Strategy
+import com.inc.pmu.BuildConfig
+import java.util.*
+
+class ViewModelHost(private val connectionsClient: ConnectionsClient) : ViewModel() {
+    private val localUsername = UUID.randomUUID().toString()
+    private var localPlayer: Int = 0
+    private var playersEndpointIds = mutableListOf<String>()
+
+    private companion object {
+        const val TAG = "PMU"
+        val STRATEGY = Strategy.P2P_STAR
+    }
+
+
+    fun startHosting() {
+        Log.d(TAG, "Start advertising...")
+        val advertisingOptions = AdvertisingOptions.Builder().setStrategy(STRATEGY).build()
+        connectionsClient.startAdvertising(
+            localUsername, // 2
+            BuildConfig.APPLICATION_ID, // 3
+            connectionLifecycleCallback, // 4
+            advertisingOptions // 5
+        ).addOnSuccessListener {
+            // 6
+            Log.d(TAG, "Advertising...")
+            localPlayer = 1
+        }.addOnFailureListener {
+            // 7
+            Log.d(TAG, "Unable to start advertising")
+        }
+    }
+
+    private val connectionLifecycleCallback = object : ConnectionLifecycleCallback() {
+        override fun onConnectionInitiated(endpointId: String, info: ConnectionInfo) {
+            Log.d(TAG, "onConnectionInitiated")
+
+            Log.d(TAG, "Accepting connection...")
+            connectionsClient.acceptConnection(endpointId, payloadCallback)
+        }
+
+
+        override fun onConnectionResult(endpointId: String, resolution: ConnectionResolution) {
+            Log.d(TAG, "onConnectionResult")
+
+            when (resolution.status.statusCode) {
+                ConnectionsStatusCodes.STATUS_OK -> {
+                    Log.d(TAG, "ConnectionsStatusCodes.STATUS_OK")
+
+                    playersEndpointIds.add(endpointId)
+                    Log.d(TAG, "playersEndpointIds: $playersEndpointIds")
+                }
+                ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED -> {
+                    Log.d(TAG, "ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED")
+                }
+                ConnectionsStatusCodes.STATUS_ERROR -> {
+                    Log.d(TAG, "ConnectionsStatusCodes.STATUS_ERROR")
+                }
+                else -> {
+                    Log.d(TAG, "Unknown status code ${resolution.status.statusCode}")
+                }
+            }
+        }
+
+        override fun onDisconnected(endpointId: String) {
+            Log.d(TAG, "onDisconnected")
+        }
+    }
+
+    private val payloadCallback: PayloadCallback = object : PayloadCallback() {
+        override fun onPayloadReceived(endpointId: String, payload: Payload) {
+            Log.d(TAG, "onPayloadReceived")
+        }
+
+        override fun onPayloadTransferUpdate(endpointId: String, update: PayloadTransferUpdate) {
+            Log.d(TAG, "onPayloadTransferUpdate")
+        }
+    }
+
+
+
+
+}
