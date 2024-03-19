4850bb2f360d03d04a98fe5236c97d6885f8b0ca
guillaume <guillaume.bellone02@gmail.com>
2024-03-19 14:45:37 +0100

    [refactor] activity to fragment
    added activity main class and changed pseudoChoice activity to fragment.

--- pmu/app/src/main/java/com/inc/pmu/MainActivity.kt
+++ pmu/app/src/main/java/com/inc/pmu/MainActivity.kt
@@ -1,60 +1,21 @@
 package com.inc.pmu
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var pseudoField: EditText
-    lateinit var playButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.pseudo_choice)
-
-        pseudoField = findViewById(R.id.pseudoInput)
-        playButton = findViewById(R.id.playButton)
+        setContentView(R.layout.main_activity)
     }
+
     override fun onStart() {
         super.onStart()
-
-        if (pseudoField.text.toString() != "") {
-            playButton.isClickable = true
-        }
-        else {
-            playButton.isClickable = false
-        }
-
-        pseudoField.addTextChangedListener(object: TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                if (pseudoField.text.toString() == "") {
-                    playButton.setBackgroundColor(resources.getColor(R.color.unavailable))
-                    playButton.isClickable = false
-                }
-                else {
-                    playButton.setBackgroundColor(resources.getColor(R.color.white))
-                    playButton.isClickable = true
-                }
-            }
-
-        })
+        val fragment = PseudoChoice.newInstance()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
     }
 
-    fun onClickPlayButton(view: View) {
-        val intent = Intent(this,HomePage::class.java)
-        startActivities(arrayOf(intent))
-    }
 }
---
+++ pmu/app/src/main/java/com/inc/pmu/PseudoChoice.kt
@@ -0,0 +1,63 @@
+package com.inc.pmu
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+
+class PseudoChoice : Fragment(R.layout.pseudo_choice) {
+
+    lateinit var pseudoField: EditText
+    lateinit var playButton: Button
+
+    companion object {
+        fun newInstance() = PseudoChoice()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        pseudoField = requireView().findViewById(R.id.pseudoInput)
+        playButton = requireView().findViewById(R.id.playButton)
+
+        if (pseudoField.text.toString() != "") {
+            playButton.isClickable = true
+        }
+        else {
+            playButton.isClickable = false
+        }
+
+        pseudoField.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (pseudoField.text.toString() == "") {
+                    playButton.setBackgroundColor(resources.getColor(R.color.unavailable))
+                    playButton.isClickable = false
+                }
+                else {
+                    playButton.setBackgroundColor(resources.getColor(R.color.white))
+                    playButton.isClickable = true
+                }
+            }
+
+        })
+    }
+
+    fun onClickPlayButton(view: View) {
+        val fragment = HomePage.newInstance()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
+    }
+}
