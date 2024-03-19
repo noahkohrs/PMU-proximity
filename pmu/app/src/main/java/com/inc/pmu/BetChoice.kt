e3ed71f03b8a02c5b3db1e1ed9a68719d2b1a428
Oriane Doudet <oriane.doudet@etu.univ-grenoble-alpes.fr>
2024-03-19 17:10:06 +0100

    [refactor]
    Changed BetChoice activity to Fragment

--- pmu/app/src/main/java/com/inc/pmu/BetChoice.kt
+++ pmu/app/src/main/java/com/inc/pmu/BetChoice.kt
@@ -6,54 +6,80 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.inc.pmu.models.Player
 import com.inc.pmu.models.Suit
 
-class BetChoice : AppCompatActivity() {
+class BetChoice : Fragment(R.layout.bet_choice) {
 
     var player : Player? = null
     var suit : Suit? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.bet_choice)
+
+    companion object {
+        fun newInstance() = BetChoice()
     }
 
     override fun onStart() {
         super.onStart()
         Log.d(Global.TAG, "OnStart method")
-        val playerIntent = intent
+        var buttonH : Button = requireView().findViewById(R.id.coeurButton)
+        var buttonS : Button = requireView().findViewById(R.id.piqueButton)
+        var buttonC : Button = requireView().findViewById(R.id.trefleButton)
+        var buttonD : Button = requireView().findViewById(R.id.carreauButton)
+        var buttonPlay : Button = requireView().findViewById(R.id.jouerButton)
+        //val playerIntent = intent
         //player = playerIntent.getSerializableExtra("Player") as Player
+
+        buttonH.setOnClickListener {
+            buttonH.setBackgroundColor(Color.YELLOW)
+            buttonS.setBackgroundColor(Color.WHITE)
+            buttonC.setBackgroundColor(Color.WHITE)
+            buttonD.setBackgroundColor(Color.WHITE)
+            buttonPlay.isClickable = true
+            buttonPlay.setBackgroundColor(Color.YELLOW)
+            suit = Suit.HEARTS
+        }
+
+        buttonS.setOnClickListener {
+            buttonH.setBackgroundColor(Color.WHITE)
+            buttonS.setBackgroundColor(Color.YELLOW)
+            buttonC.setBackgroundColor(Color.WHITE)
+            buttonD.setBackgroundColor(Color.WHITE)
+            buttonPlay.isClickable = true
+            buttonPlay.setBackgroundColor(Color.YELLOW)
+            suit = Suit.SPADES
+        }
+
+        buttonC.setOnClickListener {
+            buttonH.setBackgroundColor(Color.WHITE)
+            buttonS.setBackgroundColor(Color.WHITE)
+            buttonC.setBackgroundColor(Color.YELLOW)
+            buttonD.setBackgroundColor(Color.WHITE)
+            buttonPlay.isClickable = true
+            buttonPlay.setBackgroundColor(Color.YELLOW)
+            suit = Suit.CLUBS
+        }
+
+        buttonD.setOnClickListener {
+            buttonH.setBackgroundColor(Color.WHITE)
+            buttonS.setBackgroundColor(Color.WHITE)
+            buttonC.setBackgroundColor(Color.WHITE)
+            buttonD.setBackgroundColor(Color.YELLOW)
+            buttonPlay.isClickable = true
+            buttonPlay.setBackgroundColor(Color.YELLOW)
+            suit = Suit.DIAMONDS
+        }
+
+        buttonPlay.setOnClickListener {
+            val fragment = PseudoChoice.newInstance()
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit()
+        }
+
+        buttonPlay.isClickable = false
     }
 
-    fun onClickHearts(view: View) {
-        var button : Button = findViewById(R.id.coeurButton) as Button
-        button.setBackgroundColor(Color.YELLOW)
-        suit = Suit.HEARTS
-    }
 
-    fun onClickSpades(view: View) {
-        var button : Button = findViewById(R.id.piqueButton) as Button
-        button.setBackgroundColor(Color.YELLOW)
-        suit = Suit.SPADES
-    }
-
-    fun onClickClubs(view: View) {
-        var button : Button = findViewById(R.id.trefleButton) as Button
-        button.setBackgroundColor(Color.YELLOW)
-        suit = Suit.CLUBS
-    }
-
-    fun onClickDiamonds(view: View) {
-        var button : Button = findViewById(R.id.carreauButton) as Button
-        button.setBackgroundColor(Color.YELLOW)
-        suit = Suit.DIAMONDS
-    }
-
-    fun onClickJouer(view: View) {
-        intent.setClass(this,WaitingPage::class.java)
-        intent.putExtra("Player", player)
-        intent.putExtra("Suit", suit)
-        startActivities(arrayOf(intent))
-    }
 }
