ee445b5c18275eb997a796ea49aa7a473ab1f4bc
Oriane Doudet <oriane.doudet@etu.univ-grenoble-alpes.fr>
2024-03-19 15:27:00 +0100

    Auto stash before merge of "interactions_visuals" and "origin/pages_interactions"

--- pmu/app/src/main/java/com/inc/pmu/BetChoice.kt
+++ pmu/app/src/main/java/com/inc/pmu/BetChoice.kt
@@ -1,12 +1,59 @@
 package com.inc.pmu
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.inc.pmu.models.Player
+import com.inc.pmu.models.Suit
 
 class BetChoice : AppCompatActivity() {
+
+    var player : Player? = null
+    var suit : Suit? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.bet_choice)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(Global.TAG, "OnStart method")
+        val playerIntent = intent
+        //player = playerIntent.getSerializableExtra("Player") as Player
+    }
+
+    fun onClickHearts(view: View) {
+        var button : Button = findViewById(R.id.coeurButton) as Button
+        button.setBackgroundColor(Color.YELLOW)
+        suit = Suit.HEARTS
+    }
+
+    fun onClickSpades(view: View) {
+        var button : Button = findViewById(R.id.piqueButton) as Button
+        button.setBackgroundColor(Color.YELLOW)
+        suit = Suit.SPADES
+    }
+
+    fun onClickClubs(view: View) {
+        var button : Button = findViewById(R.id.trefleButton) as Button
+        button.setBackgroundColor(Color.YELLOW)
+        suit = Suit.CLUBS
+    }
+
+    fun onClickDiamonds(view: View) {
+        var button : Button = findViewById(R.id.carreauButton) as Button
+        button.setBackgroundColor(Color.YELLOW)
+        suit = Suit.DIAMONDS
+    }
+
+    fun onClickJouer(view: View) {
+        intent.setClass(this,WaitingPage::class.java)
+        intent.putExtra("Player", player)
+        intent.putExtra("Suit", suit)
+        startActivities(arrayOf(intent))
     }
 }
---
+++ pmu/app/src/main/java/com/inc/pmu/WaitingPage.kt
@@ -0,0 +1,16 @@
+package com.inc.pmu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WaitingPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.waiting_for_player)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+}
