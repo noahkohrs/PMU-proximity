7f69e654d6d90fa0fa9b23bcf597b97e908242a5
noahkohrs <noah@kohrs.fr>
2024-03-19 21:08:57 +0100

    [fix] Permissions request are back

--- pmu/app/src/main/java/com/inc/pmu/MainActivity.kt
+++ pmu/app/src/main/java/com/inc/pmu/MainActivity.kt
@@ -1,19 +1,79 @@
 package com.inc.pmu
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private companion object {
+        val REQUIRED_PERMISSIONS =
+            if (Build.VERSION.SDK_INT >= 33) {
+                arrayOf(
+                    Manifest.permission.BLUETOOTH_SCAN,
+                    Manifest.permission.BLUETOOTH_ADVERTISE,
+                    Manifest.permission.BLUETOOTH_CONNECT,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.NEARBY_WIFI_DEVICES
+                )
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                arrayOf(
+                    Manifest.permission.BLUETOOTH_SCAN,
+                    Manifest.permission.BLUETOOTH_ADVERTISE,
+                    Manifest.permission.BLUETOOTH_CONNECT,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                )
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+            } else {
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION)
+            }
+    }
+
+    private val requestMultiplePermissions = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        if (permissions.entries.any { !it.value }) {
+            Toast.makeText(this, "Required permissions needed", Toast.LENGTH_LONG).show()
+            finish()
+        } else {
+            recreate()
+        }
+    }
+
+
+    private fun hasPermissions(context: Context, permissions: Array<String>): Boolean {
+        return permissions.isEmpty() || permissions.all {
+            ContextCompat.checkSelfPermission(
+                context,
+                it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+
     }
+
 
     override fun onStart() {
         super.onStart()
         val fragment = PseudoChoice.newInstance()
+        if (!hasPermissions(this, REQUIRED_PERMISSIONS)) {
+            requestMultiplePermissions.launch(
+                REQUIRED_PERMISSIONS
+            )
+        }
         supportFragmentManager.beginTransaction()
             .replace(R.id.container, fragment)
             .addToBackStack(null)
