1e464b6e3b0d76d2419b8c2022f034f4aa26a0f0
Oriane Doudet <oriane.doudet@etu.univ-grenoble-alpes.fr>
2024-03-22 19:40:16 +0100

    [feat] Added reaction to listener onBetValidated in BetChoice

--- pmu/app/src/main/java/com/inc/pmu/viewmodels/ViewModelListener.java
+++ pmu/app/src/main/java/com/inc/pmu/viewmodels/ViewModelListener.java
@@ -1,11 +1,13 @@
 package com.inc.pmu.viewmodels;
 
+import com.inc.pmu.models.Suit;
+
 public class ViewModelListener {
     
     public void onConnectionEstablished(){}
     public void onPlayerListUpdate(String[] playerList){}
     public void onBetStart(){}
-    public void onBetValidated(){}
+    public void onBetValidated(Suit suit){}
     public void onGameCreated(){}
     public void onCardDrawn(String card){}
     public void onPlayerDoingPushUps(){}
