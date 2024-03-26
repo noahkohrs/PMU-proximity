767facad78c64e86ad3b693bc6288fae629425d6
guillaume <guillaume.bellone02@gmail.com>
2024-03-26 15:48:31 +0100

    [feat] added button listener on gameBoard

--- pmu/app/src/main/java/com/inc/pmu/GameBoard.kt
+++ pmu/app/src/main/java/com/inc/pmu/GameBoard.kt
@@ -1,10 +1,27 @@
 package com.inc.pmu
 
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.inc.pmu.viewmodels.ViewModelPMU
 
 class GameBoard : Fragment(R.layout.game_page) {
 
+    private lateinit var vmGame: ViewModelPMU
+
+    private lateinit var deckButton : Button
+
     companion object {
         fun newInstance() = GameBoard()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        deckButton = requireActivity().findViewById(R.id.deck)
+
+
+        deckButton.setOnClickListener {
+            
+        }
     }
 }
