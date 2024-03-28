3388b5e0766e0646f6091879bef963122095fbf6
Oriane Doudet <oriane.doudet@etu.univ-grenoble-alpes.fr>
2024-03-28 23:24:47 +0100

    [fix] getting divider id corresponding to the position

--- pmu/app/src/main/java/com/inc/pmu/GameBoard.kt
+++ pmu/app/src/main/java/com/inc/pmu/GameBoard.kt
@@ -1,13 +1,11 @@
 package com.inc.pmu
 
-import android.util.Log
-import android.R
+import android.app.Activity
+import android.content.Context
 import android.view.View
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.inc.pmu.models.Card
@@ -27,6 +25,9 @@ class GameBoard : Fragment(R.layout.game_page) {
     private lateinit var heart : ImageView
     private lateinit var diamonds : ImageView
 
+    private lateinit var context : Context
+
+
     companion object {
         fun newInstance() = GameBoard()
     }
@@ -36,24 +37,26 @@ class GameBoard : Fragment(R.layout.game_page) {
         super.onStart()
 
         vmGame = ViewModelProvider(requireActivity(), ViewModelPMUFactory())[ViewModelPMU::class.java]
+        context = requireContext()
+
         spades = requireView().findViewById(R.id.s1)
         club = requireView().findViewById(R.id.c1)
         heart = requireView().findViewById(R.id.h1)
         diamonds = requireView().findViewById(R.id.d1)
         deckButton = requireView().findViewById(R.id.deck)
 
-        deckButton = requireView().findViewById(R.id.deck)
-
         deckButton.setOnClickListener {
             vmGame.drawCard()
         }
+
 
         vmGame.addListener(
             object : ViewModelListener() {
                 override fun onCardDrawn(card: Card) {
                     var cardPos : Int = vmGame.game.board.riderPos.get(card.suit) as Int
-                    val id = resources.getIdentifier(("divider"+cardPos), "id", context!!.packageName)
-                    var divider : View = requireActivity().findViewById(id) as View
+                    var dividerId = getDividerFromPos(cardPos, context)
+                    var divider : View = requireView().findViewById(dividerId)
+
                     var c : ImageView
                     when(card.suit) {
                         Suit.HEARTS -> c = heart
@@ -71,5 +74,10 @@ class GameBoard : Fragment(R.layout.game_page) {
 
         deckButton.isClickable = true
 
+    }
+
+    fun getDividerFromPos(pos : Int, context : Context): Int {
+        var dividerId = context.resources.getIdentifier(("divider"+pos), null, context.packageName)
+        return dividerId
     }
 }
